d182a91172413ec4cf9f363d602f343c38f46856
Gali-Sai-Divakar-reddy <galisaidevakerreddy@gmail.com>
2023-07-01 18:19:01 +0530

    completed course app with jwt authentication

--- week-3/02-course-app-easy-2/index.js
+++ week-3/02-course-app-easy-2/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require("jsonwebtoken")
 const app = express();
 
 app.use(express.json());
@@ -7,17 +8,67 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+const secretKey = "sudim34goa1u"
+
+const generateJwt = (user) => {
+  const payload = { username: user.username, };
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+};
+
+const authenticateJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const admin = req.body
+  const existingAdmin = ADMINS.find(a => a.username === admin.username)
+  if(existingAdmin){
+    res.status(403).json({error: "This admin already exists"})
+  }
+  else{
+    ADMINS.push(admin)
+    const token = generateJwt(admin)
+    res.json({message : "Admin created successfully", token: token})
+  }
 });
 
 app.post('/admin/login', (req, res) => {
   // logic to log in admin
+  const {username, password} = req.headers
+  const existingAdmin = ADMINS.find(a => a.username === username && a.password === password)
+  if(existingAdmin){
+    const token = generateJwt(existingAdmin)
+    res.json({message : "admin logged in successfully", token})
+  }
+  else{
+    res.status(403).json({error: "admin authentication failed"})
+  }
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post('/admin/courses', authenticateJwt,(req, res) => {
+  console.log(req.user.username)
   // logic to create a course
+  const course =  req.body
+  course.id = Date.now()
+  COURSES.push(course)
+  res.json({message: "course submitted", id: course.id})
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
